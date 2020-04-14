0c75a8ac57ef0054267f8b3a6d64c4939d2320d9
vegodiego <53877309+vegodiego@users.noreply.github.com>
2020-04-13 21:57:23 -0500

    app ok

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ class App extends Component {
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+          {this.state.list.map((i) =>
+            <li key={i}>{i}</li>
+          )}
         </ul>
       </div>
     );
